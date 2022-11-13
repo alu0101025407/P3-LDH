b8ef7866b1fbf2a8e4154b0adb2d7518f9b14c77
alu0101025407 <alu0101025407@ull.edu.es>
2022-11-13 21:22:07 +0000

    Add files via upload

---
+++ html/search/searchdata.js
@@ -0,0 +1,21 @@
+var indexSectionsWithContent =
+{
+  0: "abegmpst",
+  1: "abempt",
+  2: "bgs"
+};
+
+var indexSectionNames =
+{
+  0: "all",
+  1: "classes",
+  2: "functions"
+};
+
+var indexSectionLabels =
+{
+  0: "Todo",
+  1: "Clases",
+  2: "Funciones"
+};
+
